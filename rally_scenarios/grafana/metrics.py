f9223df1b01f5fb7606cac80bc535b2d017a3cc7
Peter Razumovsky <prazumovsky@mirantis.com>
2018-05-16 18:28:01 +0400

    Update grafana plugin to actual upstream
    Change-Id: Id32e3831e7442127f18e874a9ec129ce306f99bc

--- rally_scenarios/grafana/metrics.py
+++ rally_scenarios/grafana/metrics.py
@@ -12,56 +12,20 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import requests
-
 from rally.common import cfg
 from rally.common import logging
-from rally.common import utils as commonutils
-from rally.task import scenario as commonscenario
 from rally.task import types
 from rally.task import utils
 from rally.task import validation
 
 from rally_openstack import consts
 from rally_openstack import scenario
+from rally_scenarios.services.grafana import grafana as grafana_service
 
 CONF = cfg.CONF
 LOG = logging.getLogger(__name__)
 
 """Scenarios for Pushgateway and Grafana metrics."""
-
-
-class PushMetricBasic(commonscenario.Scenario):
-    """Utility method for push metric scenarios."""
-
-    def _check_metric(self, seed, monitor_vip, grafana, datasource_id,
-                      sleep_time, retries_total):
-        check_url = ("http://%(vip)s:%(port)s/api/datasources/proxy/:"
-                     "%(datasource)s/api/v1/query?"
-                     "query=rally_test_metric_%(seed)s" % {
-                         "vip": monitor_vip,
-                         "port": grafana["port"],
-                         "datasource": datasource_id,
-                         "seed": seed
-                     })
-        i = 0
-        LOG.info("Check metric rally_test_metric_%s in Grafana" % seed)
-        while i < retries_total:
-            LOG.info("Attempt number %s" % (i + 1))
-            resp = requests.get(check_url,
-                                auth=(grafana["user"], grafana["password"]))
-            result = resp.json()
-            LOG.info("Grafana response code: %s" % resp.status_code)
-            if len(result["data"]["result"]) < 1 and i + 1 >= retries_total:
-                LOG.warning("No instance metrics found in Grafana")
-                self.assertGreater(len(result["data"]["result"]), 0)
-            elif len(result["data"]["result"]) < 1:
-                i += 1
-                commonutils.interruptable_sleep(sleep_time)
-            else:
-                LOG.warning("Metric instance found in Grafana")
-                self.assertGreater(len(result["data"]["result"]), 0)
-                break
 
 
 @types.convert(image={"type": "glance_image"},
@@ -71,7 +35,7 @@ class PushMetricBasic(commonscenario.Scenario):
 @scenario.configure(context={"cleanup@openstack": ["nova"]},
                     name="GrafanaMetrics.push_metric_from_instance",
                     platform="openstack")
-class PushMetricsInstance(PushMetricBasic, scenario.OpenStackScenario):
+class PushMetricsInstance(scenario.OpenStackScenario):
     """Test monitoring system by pushing metric from nova server and check it.
 
     Scenario tests monitoring system, which uses Pushgateway as metric exporter
@@ -88,13 +52,15 @@ class PushMetricsInstance(PushMetricBasic, scenario.OpenStackScenario):
     data.
     """
 
-    def _push_metric(self, seed, image, flavor, monitor_vip, pushgateway_port):
+    def _metric_from_instance(self, seed, image, flavor, monitor_vip,
+                              pushgateway_port, job_name):
         push_cmd = (
-            "echo rally_test_metric_%(seed)s 12345 | curl --data-binary "
+            "echo %(seed)s 12345 | curl --data-binary "
             "@- http://%(monitor_vip)s:%(pgtw_port)s/metrics/job"
-            "/rally_test" % {"seed": seed,
-                             "monitor_vip": monitor_vip,
-                             "pgtw_port": pushgateway_port})
+            "/%(job_name)s" % {"seed": seed,
+                               "monitor_vip": monitor_vip,
+                               "pgtw_port": pushgateway_port,
+                               "job_name": job_name})
         userdata = ("#!/bin/bash\n%s" % push_cmd)
         server = self.clients("nova").servers.create(seed,
                                                      image, flavor,
@@ -112,7 +78,8 @@ class PushMetricsInstance(PushMetricBasic, scenario.OpenStackScenario):
                  "active" % seed)
 
     def run(self, image, flavor, monitor_vip, pushgateway_port,
-            grafana, datasource_id, sleep_time=5, retries_total=30):
+            grafana, datasource_id, job_name, sleep_time=5,
+            retries_total=30):
         """Create nova instance with pushing metric script as userdata.
 
         Push metric to metrics storage using Pushgateway and check it in
@@ -125,23 +92,34 @@ class PushMetricsInstance(PushMetricBasic, scenario.OpenStackScenario):
         :param grafana: Grafana dict with creds and port to use for checking
                metric. Format: {user: admin, password: pass, port: 9902}
         :param datasource_id: metrics storage datasource ID in Grafana
+        :param job_name: job name to push metric in it
         :param sleep_time: sleep time between checking metrics in seconds
         :param retries_total: total number of retries to check metric in
                               Grafana
         """
         seed = self.generate_random_name()
 
-        self._push_metric(seed, image, flavor, monitor_vip, pushgateway_port)
-        self._check_metric(seed, monitor_vip, grafana, datasource_id,
-                           sleep_time, retries_total)
+        grafana_svc = grafana_service.GrafanaService(
+            self._clients,
+            name_generator=self.generate_random_name,
+            atomic_inst=self.atomic_actions())
+
+        self._metric_from_instance(seed, image, flavor, monitor_vip,
+                                   pushgateway_port, job_name)
+        checked = grafana_svc.check_metric(seed, monitor_vip=monitor_vip,
+                                           grafana=grafana,
+                                           datasource_id=datasource_id,
+                                           sleep_time=sleep_time,
+                                           retries_total=retries_total)
+        self.assertTrue(checked)
 
 
 @scenario.configure(name="GrafanaMetrics.push_metric_locally")
-class PushMetricLocal(PushMetricBasic, commonscenario.Scenario):
+class PushMetricLocal(scenario.OpenStackScenario):
     """Test monitoring system availability with local pushing random metric."""
 
     def run(self, monitor_vip, pushgateway_port, grafana, datasource_id,
-            sleep_time=5, retries_total=30):
+            job_name, sleep_time=5, retries_total=30):
         """Push random metric to Pushgateway locally and check it in Grafana.
 
         :param monitor_vip: monitoring system IP to push metric
@@ -149,23 +127,25 @@ class PushMetricLocal(PushMetricBasic, commonscenario.Scenario):
         :param grafana: Grafana dict with creds and port to use for checking
                metric. Format: {user: admin, password: pass, port: 9902}
         :param datasource_id: metrics storage datasource ID in Grafana
+        :param job_name: job name to push metric in it
         :param sleep_time: sleep time between checking metrics in seconds
         :param retries_total: total number of retries to check metric in
                               Grafana
         """
         seed = self.generate_random_name()
 
-        push_url = "http://%(ip)s:%(port)s/metrics/job/rally_test" % {
-            "ip": monitor_vip,
-            "port": pushgateway_port
-        }
-        resp = requests.post(push_url,
-                             headers={"Content-type": "text/xml"},
-                             data="rally_test_metric_%s 12345\n" % seed)
-        if resp.ok:
-            LOG.info("Metric %s pushed locally")
-        else:
-            LOG.error("Error during push metric %s")
-            self.assertFalse(resp.ok)
-        self._check_metric(seed, monitor_vip, grafana, datasource_id,
-                           sleep_time, retries_total)
+        grafana_svc = grafana_service.GrafanaService(
+            self._clients,
+            name_generator=self.generate_random_name,
+            atomic_inst=self.atomic_actions())
+
+        pushed = grafana_svc.push_metric(seed, monitor_vip=monitor_vip,
+                                         pushgateway_port=pushgateway_port,
+                                         job_name=job_name)
+        self.assertTrue(pushed)
+        checked = grafana_svc.check_metric(seed, monitor_vip=monitor_vip,
+                                           grafana=grafana,
+                                           datasource_id=datasource_id,
+                                           sleep_time=sleep_time,
+                                           retries_total=retries_total)
+        self.assertTrue(checked)
