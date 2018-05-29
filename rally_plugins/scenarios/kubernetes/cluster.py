bcbab178921219009d2c672066bae0bd48f4680c
Peter Razumovsky <prazumovsky@mirantis.com>
2018-05-29 21:52:06 +0400

    Second and initial kubernetes plugin
    Kubernetes plugin with existing kubernetes platform
    and one workable test.
    
    Change-Id: I451299de8349251eb69ddb705029bbe2bc2b7a3e

--- rally_plugins/scenarios/kubernetes/cluster.py
+++ rally_plugins/scenarios/kubernetes/cluster.py
@@ -12,52 +12,83 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from rally.common import logging
+import random
+
 from rally.task import scenario
-from rally.task import types
-from rally.task import utils
-from rally.task import validation
-
-from rally_openstack import consts
 from rally_plugins.services.kube import kube
-
-LOG = logging.getLogger(__name__)
 
 
 @scenario.configure(name="Kubernetes.run_namespaced_pods",
-                    platform="openstack")
+                    platform="kubernetes")
 class NamespacedPodsPlugin(scenario.Scenario):
+    """Kubernetes pods sequence create and delete test.
 
-    def _cleanup(self, client, pods, namespace):
+    Choose created namespace, create defined number of pods, wait until they
+    not be running and delete them after.
+    """
+
+    def __init__(self, context=None):
+        super(NamespacedPodsPlugin, self).__init__(context)
+        if "env" in self.context:
+            self.client = kube.KubernetesService(
+                self.context["env"]["platforms"]["kubernetes"],
+                name_generator=self.generate_random_name,
+                atomic_inst=self.atomic_actions())
+
+    def _make_event_data(self):
+        data = []
+        data_map = {}
+        data_idx = 0
+        state_map = {
+            "kube.initialized_pod": 0,
+            "kube.scheduled_pod": 1,
+            "kube.created_pod": 2
+        }
+        for e in self.client.events:
+            if data_map.get(e["pod_name"]) is None:
+                data_map[e["pod_name"]] = data_idx
+                data.append([e["pod_name"], []])
+                data_idx += 1
+            duration = e["finished_at"] - e["started_at"]
+            data[data_map[e["pod_name"]]][1].append([state_map[e["name"]],
+                                                     duration])
+        return data
+
+    def _cleanup(self, pods, namespace):
         for pod in pods:
-            client.delete_pod(pod, namespace=namespace)
-        client.delete_namespace(namespace)
+            self.client.delete_pod(pod, namespace=namespace)
+
+    def _choose_namespace(self):
+        if self.context["namespace_choice_method"] == "random":
+            return random.choice(self.context["namespaces"])
+        elif self.context["namespace_choice_method"] == "round_robin":
+            return self.context["namespaces"][self.context["iteration"] - 1]
 
     def run(self, pods_number, image, sleep_time=5, retries_total=30):
-        client = kube.KubernetesService(
-            name_generator=self.generate_random_name,
-            atomic_inst=self.atomic_actions()
-        )
+        """Create number of pods and delete them.
 
-        namespace = self.generate_random_name().replace('_', '-').lower()
-        self.assertTrue(client.create_namespace(namespace,
-                                                sleep_time=sleep_time,
-                                                retries_total=retries_total))
-        LOG.info("Namespace %s is active" % namespace)
-
+        :param pods_number: total number of pods in sequence
+        :param image: image used in pods manifests
+        :param sleep_time: sleep time between each two retries
+        :param retries_total: total number of retries
+        """
+        namespace = self._choose_namespace()
         pods = []
         for i in range(pods_number):
-            pod_name = self.generate_random_name().replace('_', '-').lower()
-            self.assertTrue(client.create_pod(pod_name, image=image,
-                                              namespace=namespace,
-                                              sleep_time=sleep_time,
-                                              retries_total=retries_total))
+            pod_name = self.generate_random_name().replace('_',
+                                                           '-').lower()
+            self.assertTrue(self.client.create_pod(
+                pod_name, image=image, namespace=namespace,
+                sleep_time=sleep_time, retries_total=retries_total))
             pods.append(pod_name)
+        self._cleanup(pods, namespace)
 
-        for pod in pods:
-            self.assertTrue(client.wait_pod_running(
-                pod, namespace=namespace, sleep_time=sleep_time,
-                retries_total=retries_total))
-
-        LOG.info("Cleanup pods and namespace")
-        self._cleanup(client, pods, namespace)
+        data = self._make_event_data()
+        self.add_output(
+            complete={"title": "Pods conditions total duration",
+                      "description": "Pod(s sequence): %s" % " => ".join(pods),
+                      "chart_plugin": "StackedArea",
+                      "data": data,
+                      "label": "Total seconds",
+                      "axis_label": "Pod conditions (0 - Initialize, "
+                                    "1 - Scheduled, 2 - Ready)"})
--- rally_plugins/services/kube/kube.py
+++ rally_plugins/services/kube/kube.py
@@ -11,11 +11,11 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import time
 
 from kubernetes import config
 from kubernetes import client
 from kubernetes.client.apis import core_v1_api
-from kubernetes.client import rest
 from rally.common import logging
 from rally.common import utils as commonutils
 from rally.task import atomic
@@ -26,20 +26,19 @@ LOG = logging.getLogger(__name__)
 
 class KubernetesService(service.Service):
 
-    def __init__(self, name_generator=None, atomic_inst=None):
+    def __init__(self, spec, name_generator=None, atomic_inst=None):
         super(KubernetesService, self).__init__(None,
                                                 name_generator=name_generator,
                                                 atomic_inst=atomic_inst)
-        self.api = core_v1_api.CoreV1Api()
+        apiclient = config.new_client_from_config(spec.get("config_file"))
+        self.api = core_v1_api.CoreV1Api(api_client=apiclient)
+        self.events = []
 
     @atomic.action_timer("kube.create_namespace")
-    def create_namespace(self, name, sleep_time=1, retries_total=30):
-        """Create namespace and wait until it's not active.
+    def create_namespace(self, name):
+        """Create namespace.
 
         :param name: namespace name
-        :param sleep_time: sleep time between iterations of waiting
-        :param retries_total: total count of retries before create failed
-        :returns True if namespace created successfully and False otherwise
         """
         manifest = {
             "apiVersion": "v1",
@@ -51,23 +50,87 @@ class KubernetesService(service.Service):
                 }
             }
         }
-        resp = self.api.create_namespace(body=manifest)
-        LOG.info("Namespace %(name)s created. Status: %(status)s" % {
-            "name": name,
-            "status": resp.status.phase
-        })
+        self.api.create_namespace(body=manifest)
+        LOG.info("Namespace %s created" % name)
 
-        i = 0
-        LOG.debug("Wait until namespace status not active")
-        while i < retries_total:
-            LOG.debug("Attempt number %s" % i)
-            resp = self.api.read_namespace(name)
-            if resp.status.phase != "Active":
-                i += 1
-                commonutils.interruptable_sleep(sleep_time)
-            else:
-                return True
-        return False
+    @atomic.action_timer("kube.create_serviceaccount")
+    def create_serviceaccount(self, name, namespace):
+        """ Create serviceaccount and token for namespace.
+
+        :param name: serviceaccount name
+        :param namespace: namespace where sa should be created.
+        """
+        sa_manifest = {
+           "apiVersion": "v1",
+           "kind": "ServiceAccount",
+           "metadata": {
+                "name": name,
+                "labels": {
+                    "role": "rally-test"
+                }
+           }
+        }
+        self.api.create_namespaced_service_account(namespace=namespace,
+                                                   body=sa_manifest)
+        LOG.info("ServiceAccount %s created." % name)
+        secret_manifest = {
+            "apiVersion": "v1",
+            "kind": "Secret",
+            "metadata": {
+                "name": name,
+                "labels": {
+                    "role": "rally-test"
+                },
+                "annotations": {
+                    "kubernetes.io/service-account.name": name
+                }
+            }
+        }
+        self.api.create_namespaced_secret(namespace=namespace,
+                                          body=secret_manifest)
+        LOG.info("Secret %s for service account created." % name)
+
+    def _update_pod_stats(self, name, conditions):
+        """Method for collecting pods statuses: inited, scheduled, ready."""
+        def to_time(dt):
+            return time.mktime(dt)
+
+        start, finish = None, None
+        stat_dict = {
+            "kube.initialized_pod": {},
+            "kube.scheduled_pod": {},
+            "kube.created_pod": {}
+        }
+        for d in conditions:
+            if d.type == "Initialized":
+                start = init_time = to_time(d.last_transition_time.timetuple())
+                stat_dict["kube.initialized_pod"].update({
+                    "started_at": init_time
+                })
+            elif d.type == "PodScheduled":
+                scheduled = to_time(d.last_transition_time.timetuple())
+                stat_dict["kube.scheduled_pod"].update({
+                    "started_at": scheduled
+                })
+                stat_dict["kube.initialized_pod"].update({
+                    "finished_at": scheduled
+                })
+            elif d.type == "Ready":
+                finish = to_time(d.last_transition_time.timetuple())
+                stat_dict["kube.created_pod"].update({
+                    "started_at": start,
+                    "finished_at": finish
+                })
+                stat_dict["kube.scheduled_pod"].update({
+                    "finished_at": finish
+                })
+        self.events.extend([{
+            "pod_name": name,
+            "name": k,
+            "started_at": v["started_at"],
+            "finished_at": v["finished_at"]}
+            for k, v in stat_dict.items()
+        ])
 
     @atomic.action_timer("kube.create_pod")
     def create_pod(self, name, image, namespace, sleep_time=5,
@@ -76,6 +139,9 @@ class KubernetesService(service.Service):
 
         :param name: pod's name
         :param image: pod's image
+        :param namespace: defined namespace to create pod in
+        :param sleep_time: sleep time between each two retries
+        :param retries_total: total number of retries
         :return: True if create started and False otherwise
         """
         manifest = {
@@ -88,6 +154,7 @@ class KubernetesService(service.Service):
                 }
             },
             "spec": {
+                "serviceAccountName": namespace,
                 "containers": [
                     {
                         "name": name,
@@ -98,6 +165,7 @@ class KubernetesService(service.Service):
         }
 
         i = 0
+        create_started = False
         while i < retries_total:
             LOG.debug("Attempt number %s" % i)
             try:
@@ -107,38 +175,35 @@ class KubernetesService(service.Service):
                     "name": name,
                     "status": resp.status.phase
                 })
-            except rest.ApiException as ex:
-                LOG.error("Pod create failed with status %(status)s: %(msg)s" % {
-                    "status": ex.status,
-                    "msg": ex.message
-                })
+            except Exception as ex:
+                LOG.error("Pod create failed: %s" % ex.message)
                 i += 1
                 commonutils.interruptable_sleep(sleep_time)
             else:
-                return True
-        return False
+                create_started = True
+                break
 
-    @atomic.action_timer("kube.wait_pod_running")
-    def wait_pod_running(self, name, namespace, sleep_time=1,
-                         retries_total=30):
-        """Wait until pod status not running.
+        if not create_started:
+            return False
 
-        :param name: pod's name
-        :param namespace: pod's namespace
-        :param sleep_time: sleep time between requesting pod's status.
-        :param retries_total: total count of retries before method failed.
-        :return: True if pod status becomes 'Running' and False otherwise
-        """
         i = 0
         LOG.debug("Wait until pod status not running")
         while i < retries_total:
             LOG.debug("Attempt number %s" % i)
-            resp = self.api.read_namespaced_pod(name=name, namespace=namespace)
-            if resp.status.phase != "Running":
+            try:
+                resp = self.api.read_namespaced_pod(name=name,
+                                                    namespace=namespace)
+            except Exception as ex:
+                LOG.warning("Unable to read pod status: %s" % ex.message)
                 i += 1
                 commonutils.interruptable_sleep(sleep_time)
             else:
-                return True
+                if resp.status.phase != "Running":
+                    i += 1
+                    commonutils.interruptable_sleep(sleep_time)
+                else:
+                    self._update_pod_stats(name, resp.status.conditions)
+                    return True
         return False
 
     @atomic.action_timer("kube.delete_pod")
