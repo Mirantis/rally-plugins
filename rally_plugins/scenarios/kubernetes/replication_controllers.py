acdccccc62205065bd518c39daaa8b34fbd5117f
Peter Razumovsky <prazumovsky@mirantis.com>
2018-06-07 00:25:30 +0400

    Separate different plugins on different modules
    Separate RC and pod plugins on different scenario modules.
    
    Change-Id: I487ca1b52132f43bebc5f2e20a9433704a1c35b7

--- rally_plugins/scenarios/kubernetes/replication_controllers.py
+++ rally_plugins/scenarios/kubernetes/replication_controllers.py
@@ -17,62 +17,7 @@ from rally.task import scenario
 
 from rally_plugins.scenarios.kubernetes import common
 
-
 LOG = logging.getLogger(__name__)
-
-
-@scenario.configure(name="Kubernetes.run_namespaced_pod",
-                    platform="kubernetes")
-class NamespacedPodPlugin(common.KubernetesScenario, scenario.Scenario):
-    """Kubernetes pod create and delete test.
-
-    Choose created namespace, create pod with defined image, wait until it
-    not be running and delete it after.
-    """
-
-    def _make_event_data(self):
-        data = [[] for _ in range(3)]
-        state_map = {
-            "kube.initialized_pod": 0,
-            "kube.scheduled_pod": 1,
-            "kube.created_pod": 2
-        }
-
-        for e in self.client.events:
-            duration = e["finished_at"] - e["started_at"]
-            data[state_map[e["name"]]] = [e["name"], duration]
-        return data
-
-    def run(self, image, sleep_time=5, retries_total=30):
-        """Create pod and then delete it.
-
-        :param image: image used in pods manifests
-        :param sleep_time: sleep time between each two retries
-        :param retries_total: total number of retries
-        """
-        namespace = self._choose_namespace()
-        pod = self.generate_name()
-
-        # create
-        self.assertTrue(self.client.create_pod(pod, image=image,
-                                               namespace=namespace,
-                                               sleep_time=sleep_time,
-                                               retries_total=retries_total))
-
-        # cleanup
-        self.assertTrue(self.client.delete_pod(pod, namespace=namespace,
-                                               sleep_time=sleep_time,
-                                               retries_total=retries_total))
-
-        data = self._make_event_data()
-        self.add_output(
-            additive={"title": "Pod's conditions total duration",
-                      "description": "Total durations for pod in each "
-                                     "iteration",
-                      "chart_plugin": "StackedArea",
-                      "data": data,
-                      "label": "Total seconds",
-                      "axis_label": "Iteration"})
 
 
 @scenario.configure(name="Kubernetes.create_delete_replication_controller",
